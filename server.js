c6c40252d8a9fdabc65567ba8d17212a3306ef4e
Isaiah Henson <Devpuddles@gmail.com>
2022-03-03 06:22:14 -0500

    connected script and link to ejs file server side

--- server.js
+++ server.js
@@ -26,6 +26,10 @@ app.use(express.urlencoded({extended: true}))
 app.use(cors())
 app.use(express.json())
 
+app.get('/', (request, response) => {
+    response.render('index.ejs')
+})
+
 app.listen(process.env.PORT || PORT, () => {
     console.log(`listening on port ${PORT}`)
 })
